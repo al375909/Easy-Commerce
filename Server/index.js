774af8aabf5c6a7aa6e1ffffafd728fc9129d451
carlos <al376028@uji.es>
2021-03-24 16:33:40 +0100

    añadido bloque de react en backend

--- Server/index.js
+++ Server/index.js
@@ -1,6 +1,17 @@
 const express = require('express')
 const app = express();
 
+const path = require('path');
+if (process.env.NODE_ENV === 'production') {
+  // Serve any static files
+  app.use(express.static(path.join(__dirname, 'client/build')));
+// Handle React routing, return all requests to React app
+  app.get('*', function(req, res) {
+    res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+  });
+}
+
+
 app.get('/', (req, res) => {
   res.send('Hello World!')
 });
