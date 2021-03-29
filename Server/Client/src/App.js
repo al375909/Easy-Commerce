50b401dd2f274c70e8488cc459cc943e1e508c16
dani10mv <danielmirallesvargas@gmail.com>
2021-03-29 17:01:51 +0200

    Header añadido

--- Server/Client/src/App.js
+++ Server/Client/src/App.js
@@ -1,7 +1,7 @@
 import './App.css';
-import Comunicacion from './components/comunicacionPrueba';
 import { useEffect } from 'react';
 import TiendasProvider from './context/tiendas/Provider.js';
+import Home from './views/Home';
 
 
 function App() {
@@ -9,7 +9,7 @@ function App() {
   useEffect(() => {
 
     // Usa la función gteDato donde se setea la varibale datos al renderizar el componente
-    console.log(TiendasProvider)
+  
 
     // es necesario injectar dependencias en useEffect para que pueda usar le metodo del hook que usamos
   }, []);
@@ -17,7 +17,7 @@ function App() {
 
   return (
     <TiendasProvider>
-      <Comunicacion />
+      <Home />
     </TiendasProvider>
     
 
