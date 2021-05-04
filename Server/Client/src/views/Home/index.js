b2fd66106ea0e0340a685bf30a498ebd83d7a1bd
sanahujapablo <pablosana99@gmail.com>
2021-05-04 16:41:47 +0200

    Refactor de los botones del listado de comercios

--- Server/Client/src/views/Home/index.js
+++ Server/Client/src/views/Home/index.js
@@ -27,7 +27,7 @@ export default function Home() {
         <div>
             <Header/>
             <Commerces datos={datos}/>
-            <AddProductView/>
+            {/* <AddProductView/> Pablo la lio aqui, no tocar*/}
         </div>
 
     );
