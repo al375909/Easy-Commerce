fe2c3e1c7c7195f09b1ef943bf70dae0abdf2ce4
Marcos <al375909@uji.es>
2021-05-05 16:30:47 +0200

    ajuste css

--- Server/Client/src/components/Header/index.js
+++ Server/Client/src/components/Header/index.js
@@ -21,95 +21,88 @@ export default function Header(props) {
     }
 
 
-    return (
-        <>
-            <div className={
-                `left-menu ${
-                    menuOpen ? "open" : "closed"
-                }`
-            }>
-                <button type="button"
-                    onClick={
-                        () => {
-                            setMenutOpen(!menuOpen)
-                        }
-                }>close</button>
-                <div className="links">
-                    <ul>
-                        <li></li>
-                    </ul>
-                </div>
+    return (<>
+        <div className={
+            `left-menu ${
+                menuOpen ? "open" : "closed"
+            }`
+        }>
+            <button type="button"
+                onClick={
+                    () => {
+                        setMenutOpen(!menuOpen)
+                    }
+            }>close</button>
+            <div className="links">
+                <ul>
+                    <li></li>
+                </ul>
+            </div>
+        </div>
+
+        <nav className="navbar navbar-expand-lg navbar-light bg-light sticky-header"> {/* Burger menu */}
+            <button className="burger-menu"
+                onClick={
+                    () => {
+                        setMenutOpen(!menuOpen)
+                    }
+                }
+                type="button">
+                <span className="navbar-toggler-icon"></span>
+            </button>
+
+            {/* Logo */}
+            <div className="navbar-brand">
+                <a href="/">
+                    <img alt=''
+                        src={cesta}/>
+                </a>
             </div>
 
-            <nav className="navbar navbar-expand-lg navbar-light bg-light">
-                {/* Burger menu */}
-                <button className="burguer-menu"
-                    onClick={
-                        () => {
-                            setMenutOpen(!menuOpen)
-                        }
-                    }
-                    type="button">
-                    <span className="navbar-toggler-icon"></span>
-                </button>
+            {/* Full width menu */}
+            <div className="navbar-links collapse navbar-collapse" id="navbarSupportedContent">
+                <ul className="navbar-nav mr-auto">
+                    <li className="nav-item active">
+                        <a className="nav-link" href="#">Home
+                            <span className="sr-only">(current)</span>
+                        </a>
+                    </li>
+                    <li className="nav-item">
+                        <a className="nav-link" href="#">Mis pedidos</a>
+                    </li>
+                </ul>
+            </div>
 
-                {/* Logo */}
-                <div className="navbar-brand">
-                    <a href="/">
-                        <img alt=''
-                            src={cesta}/>
-                    </a>
-                </div>
+            {/* Cart + User Pic */}
+            <div className="navbar-user"> {
+                user ? <div className="user-box">
 
-                {/* Full width menu */}
-                <div className="navbar-links collapse navbar-collapse" id="navbarSupportedContent">
-                    <ul className="navbar-nav mr-auto">
-                        <li className="nav-item active">
-                            <a className="nav-link" href="#">Home
-                                <span className="sr-only">(current)</span>
-                            </a>
-                        </li>
-                        <li className="nav-item">
-                            <a className="nav-link" href="#">Mis pedidos</a>
-                        </li>
-                    </ul>
-                </div>
-
-                {/* Cart + User Pic */}
-                <div className="navbar-user">
-
-
-                    {
-                      user ? <div className="user-box">
-
-                        <div className="cart-box">
-                            <div className="cart">
-                                <img src={cart}/>
-                            </div>
-
-                            <div className="cart-products">
-                                <p>4</p>
-                            </div>
+                    <div className="cart-box">
+                        <div className="cart">
+                            <img src={cart}/>
                         </div>
 
-                        <div className="user-pic">
-                            <img alt='' className="profile-pic"
-                                src={userpic}/>
+                        <div className="cart-products">
+                            <p>4</p>
                         </div>
-                        <div className="logout">
-                            <Link to="/">
-                                <button onClick={logout}>Logout</button>
-                            </Link>
-                        </div>
-                    </div> : <div className="login">
-                        <Link to="/login">
-                            <button>Login</button>
+                    </div>
+
+                    <div className="user-pic">
+                        <img alt='' className="profile-pic"
+                            src={userpic}/>
+                    </div>
+                    <div className="logout">
+                        <Link to="/">
+                            <button onClick={logout}>Logout</button>
                         </Link>
                     </div>
-                } </div>
-            </nav>
-        </>
-
-    );
+                </div> : <div className="login">
+                    <Link to="/login">
+                        <button>Login</button>
+                    </Link>
+                </div>
+            } </div>
+        </nav>
+    </>);
 
 }
